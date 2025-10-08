acff6152d760585e0bcbd9bdc733fac90febd202
83-TechWorld <sathishkumar.kanda@gmail.com>
2025-10-08 05:44:24 +0530

    init scripts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { StoreProvider } from 'easy-peasy';
-import App from './App.tsx';
-import { store } from './store/chatStore';
-
-// Import Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
-// Import Bootstrap Icons
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import './index.css';
+import App from './App';
+import { store } from './store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <StoreProvider store={store}>
       <App />
     </StoreProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
